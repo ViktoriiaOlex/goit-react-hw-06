a5c7ad89610ff6da8ed91b0313df74c7742e5dee
Victoriia Oleh <slavika86@gmail.com>
2024-04-12 17:55:56 +0300

    add project

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
+import css from './SearchBox.module.css';
+import { getFilterName } from '../../redux/selectors';
+import { changeFilter } from '../../redux/filtersSlice';
+
+const SearchBox = () => {
+  const dispatch = useDispatch();
+  const getFilterVal = useSelector(getFilterName);
+  const handleImputFilterVal = ev => {
+    dispatch(changeFilter(ev.target.value));
+  };
+  return (
+    <div className={css.container}>
+      <p>Find contacts by name</p>
+      <input
+        className={css.input}
+        value={getFilterVal}
+        onChange={handleImputFilterVal}
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
